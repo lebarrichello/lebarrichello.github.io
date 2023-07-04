aca2ac76a92c455252a347befa6534e66f0a3105
lebarrichello <leticia.barrichello@gmail.com>
2023-07-04 17:24:49 -0300

    :tada: Commit inicial

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+/* import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+ 
+  render() {
+
+    return (
+<header className="container__header">
+<div className="header__logo-name">
+   <p>lebarrichello</p>
+</div>
+<div className="header__navigation-links">
+    <nav>
+        <p>Sobre</p>
+        <p>Projetos</p>
+        <p>Contato</p>
+    </nav>
+</div>
+<div className="header__toogle-theme">light/dark theme </div>
+</header>);
+  }
+}
+
+export default Header;
+
+ */
