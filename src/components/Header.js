7579b04e9f7b64d558e94d3fd5cc2397c0962085
lebarrichello <leticia.barrichello@gmail.com>
2023-07-04 20:00:05 -0300

    :sparkles:feat: Adicionando efeito em texto

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isMenuOpen: false
+      isMenuOpen: false,
+      logoText: 'lebarrichello',
+      blinking: false
     };
   }
 
@@ -16,14 +18,47 @@ class Header extends Component {
     }));
   };
 
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.addLetterT();
+    }, 20000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  addLetterT = () => {
+    const { logoText } = this.state;
+
+    if (!logoText.includes('t')) {
+      this.setState({
+        logoText: logoText.slice(0, 2) + 't' + logoText.slice(2),
+        blinking: true
+      });
+
+      setTimeout(() => {
+        this.setState({ blinking: false });
+        setTimeout(() => {
+          this.setState({ logoText: 'lebarrichello' });
+        }, 1000);
+      }, 5000);
+    }
+  };
+
   render() {
-    const { isMenuOpen } = this.state;
+    const { isMenuOpen, logoText, blinking } = this.state;
 
     return (
       <header className="container__header">
         <div className="header__logo-name">
           <span>{"<"}</span>
-          <p>lebarrichello</p>
+          <p className="logo-text">
+            <span className={blinking ? 'blinking' : ''}>{logoText.charAt(0)}</span>
+            <span className={blinking ? 'blinking' : ''}>{logoText.charAt(1)}</span>
+            <span className={blinking ? 'blinking' : ''}>{logoText.charAt(2)}</span>
+            {logoText.slice(3)}
+          </p>
           <span>{">"}</span>
         </div>
         <nav className={`header__navigation-links ${isMenuOpen ? 'open' : ''}`}>
