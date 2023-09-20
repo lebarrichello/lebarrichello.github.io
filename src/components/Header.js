89b4b5ea54d4b049afcb557cff71847a777a6533
lebarrichello <leticia.barrichello@gmail.com>
2023-09-20 17:21:00 -0300

    estilizacao componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import { RiMenuFill } from 'react-icons/ri';
-import { RxCross2 } from "react-icons/rx";
+import { RxCross2 } from 'react-icons/rx';
 import { useDarkMode } from '../context/DarkModeContext';
 import '../styles/Header.css';
 
@@ -21,8 +21,8 @@ function Header() {
     }, 20000);
 
     return () => clearInterval(interval);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   const addLetterT = () => {
     if (!logoText.includes('t')) {
       setLogoText(logoText.slice(0, 2) + 't' + logoText.slice(2));
@@ -34,6 +34,14 @@ function Header() {
           setLogoText('lebarrichello');
         }, 1000);
       }, 5000);
+    }
+  };
+
+  // Função para rolar para a seção correspondente quando um link de navegação for clicado
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
@@ -50,14 +58,15 @@ function Header() {
         <h2>{">"}</h2>
       </div>
       <nav className={`header__navigation-links ${isMenuOpen ? 'open' : ''}`}>
-        <a href="#about">Sobre</a>
-        <a href="#projects">Projetos</a>
+        {/* Use a função scrollToSection ao clicar nos links */}
+        <button onClick={() => scrollToSection('about')}>Sobre</button>
+        <button onClick={() => scrollToSection('projects')}>Projetos</button>
         <a href="https://linkedin.com/in/lebarrichello" target="_blank" rel="noreferrer">
           Contato
         </a>
       </nav>
       <div className="header__toggle-theme" onClick={toggleDarkMode}>
-        {isDarkMode ? <MdLightMode  className="icon" /> : <MdDarkMode className="icon" />}
+        {isDarkMode ? <MdLightMode className="icon" /> : <MdDarkMode className="icon" />}
       </div>
       <div className="header__hamburger-menu" onClick={toggleMenu}>
         {isMenuOpen ? <RxCross2 className="icon" /> : <RiMenuFill className="icon" />}
